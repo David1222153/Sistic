dee840c7e65c4a9bc80f40a35201196a6baa182d
David1222153 <1222153@isep.ipp.pt>
2025-03-26 20:57:01 +0000

    ficha exercicos 3 completa

--- ficha exercicios 3/sinais-iii.c
+++ ficha exercicios 3/sinais-iii.c
@@ -1,5 +1,7 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 #include <sys/wait.h>
 
 void wait_something();
@@ -7,30 +9,59 @@ void do_something();
 void myHandler(int);
 
 int main() {
-  while(1) {
-   wait_something();
-   pid_t r = fork();
-   if(r == 0) {
-     do_something();
-     return(0);
-   }
-  }
-  return(0);
+  printf("1222153 - %s\n", __FILE__);
+    // Bloquear SIGINT e SIGQUIT no processo principal
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGINT);
+    sigaddset(&mask, SIGQUIT);
+    sigprocmask(SIG_BLOCK, &mask, NULL);
+
+    // Ignorar SIGTERM em todos os processos
+    signal(SIGTERM, SIG_IGN);
+
+    // Definir handler para SIGCHLD no processo inicial
+    signal(SIGCHLD, myHandler);
+
+    while(1) {
+        wait_something();
+        pid_t r = fork();
+        
+        if (r == -1) {
+          perror("Erro ao criar processo");
+          exit(1);
+      }
+
+        if (r == 0) {  // Processo filho
+            // Bloquear SIGTSTP apenas no filho
+            sigset_t child_mask;
+            sigemptyset(&child_mask);
+            sigaddset(&child_mask, SIGTSTP);
+            sigprocmask(SIG_BLOCK, &child_mask, NULL);
+
+            // Restaurar SIGCHLD para o comportamento padrão no filho
+            signal(SIGCHLD, SIG_DFL);
+
+            do_something();
+            return 0;
+        }
+    }
+    return 0;
 }
 
 void wait_something() {
-  sleep(1);
+    sleep(1);
 }
 
 void do_something() {
-  sleep(1);
+    sleep(1);
 }
 
+// Handler para SIGCHLD no processo pai
 void myHandler(int signum) {
-  int wstatus;
-  
-  wait(&wstatus);
-  
-  if( WIFSIGNALED(wstatus) )
-    printf("Child terminated by signal\n");
+    int wstatus;
+    wait(&wstatus);
+    if (WIFSIGNALED(wstatus)) {
+        printf("Child terminated by signal %d\n", WTERMSIG(wstatus));
+    }
 }
--- ficha exercicios 3/sinais-iv.c
+++ ficha exercicios 3/sinais-iv.c
@@ -1,31 +1,45 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+// Função que evita processos zombi
+void handle_sigchld(int signum) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
 void fun1(int *d) {
-  ++d[0];
-  printf("Novo valor gerado em fun1: %d\n", d[0]);
-  sleep(2);
+    (*d)++;
+    printf("Novo valor gerado: %d\n", *d);
+    sleep(2);
 }
 
 void fun2(int *d) {
-  sleep(1); 
-  printf("Valor processado por fun2: %d\n", d[0]);
+    sleep(1);
+    printf("Valor processado: %d\n", *d);
 }
 
-int main(){
-  int dados = 0;
+int main() {
+  printf("1222153 - %s\n", __FILE__);
+    int dados = 0;
 
-  while(1) {
-    fun1(&dados);
-    pid_t r = fork();
-    if(r == -1) {
-      perror("fork");
-      exit(1);
+    // Configurar o sinal SIGCHLD para evitar processos zumbi
+    signal(SIGCHLD, handle_sigchld);
+
+    while (1) {
+        fun1(&dados);
+        pid_t r = fork();
+
+        if (r == -1) {
+            perror("fork");
+            exit(1);
+        }
+
+        if (r == 0) {  // Código do processo filho
+            fun2(&dados);
+            exit(0);
+        }
     }
-    if(r==0) {
-      fun2(&dados);
-      exit(0);
-    }
-  }
 }
+
